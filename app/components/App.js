fce386897896d4f9c6220477050d73156a2985a7
Aya Peacock <ayapcck@gmail.com>
2019-04-24 23:37:01 +0000

    Created center panel

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,14 @@
 var React = require('react');
 
+import CenterPanel from './centerPanel/centerPanel.js';
+
 import styles from './style.css';
 
-export default class Greeting extends React.Component {
-  render(){
-    return <div class={styles.box}>Hello World Today!</div>;
+export default class App extends React.Component {
+  render() {
+	var app = <div className={styles.container}>
+		<CenterPanel />
+	</div>
+    return app;
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import App from './components/App.js';
 
+import styles from './index.css';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
+  document.getElementById('body')
 );
