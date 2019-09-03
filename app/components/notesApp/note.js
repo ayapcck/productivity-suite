5cd4fcf548a4de7b2fc1c2ce832473fdb5d11782
Aya Peacock <ayapcck@gmail.com>
2019-09-03 02:24:04 +0000

    refactored final notes to share code

--- app/components/notesApp/note.js
+++ app/components/notesApp/note.js
@@ -1,41 +1,152 @@
 import React from 'react';
 import _ from 'lodash';
+import classnames from 'classnames';
 
+import Icon from '../icons/icon';
 import { NoteSteps } from './steps';
 
 import styles from './notes.less';
+import utilStyles from '../utilities/utilities.less';
+
+const getListItemContent = () => {
+    const listItems = document.getElementsByClassName(styles.listContent);
+    let content = '';
+    _.forEach(listItems, listItem => {
+        content = `${content},${listItem.value}`;
+    });
+    content = content.slice(1);
+    return content;
+}
 
 export default class Note extends React.Component {
     constructor(props) {
         super(props);
 
+        const { content } = this.props;
+        const { id, name } = content;
+        
+        const listName = name === '' ? 'List' : name;
+
         this.state = {
+            editing: false,
+            id,
+            name: listName,
             stepName: this.props.step
         };
 
         this.changeStep = this.changeStep.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleEditingChange = this.handleEditingChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.listNameChange = this.listNameChange.bind(this);
     }
 
     changeStep(nextStep) {
         this.setState({ stepName: nextStep });
+    }    
+
+    componentDidMount() {
+        document.getElementById('content').addEventListener('mousedown', this.handleClick);
+    }
+
+    componentWillUnmount() {
+        document.getElementById('content').removeEventListener('mousedown', this.handleClick);
+    }
+
+    handleClick(ev) {
+        if (this.state.editing) {
+            const clickX = ev.clientX;
+            const clickY = ev.clientY;
+            if (clickX !== 0 && clickY !== 0) {
+                const listContainer = document.getElementsByClassName(styles.finalNote)[0];
+                const listContainerBox = listContainer.getBoundingClientRect();
+                const clickInsideContainer = clickX > listContainerBox.left 
+                    && clickX < listContainerBox.right 
+                    && clickY > listContainerBox.top 
+                    && clickY < listContainerBox.bottom;
+                !clickInsideContainer && this.handleEditingChange();
+            }
+        }
+    }
+
+    handleContentUpdate() {
+        const { id, name, stepName } = this.state;
+        let content = null;
+        if (stepName === 'list') {
+            content = getListItemContent();
+        }
+        content !== null && this.props.updateNote(content, name, id);
+    }
+
+    handleEditingChange() {
+        const { editing } = this.state;
+        if (editing) {
+            this.handleContentUpdate();
+        } else {
+
+        }
+        this.setState({ editing: !editing });
+    }
+
+    handleNameChange(ev) {
+        const { name } = this.state;
+        ev.target.value !== name && this.setState({ name: ev.target.value });
+    }
+
+    listNameChange(ev) {
+        const { name } = this.state;
+        ev.target.value !== name && this.setState({ name: ev.target.value });
     }
 
     render() {
-        const { stepName } = this.state;
+        const { content, updateNote } = this.props;
+        const { editing, name, stepName } = this.state;
         
         const currentStep = _.get(NoteSteps, stepName);
         const stepProps = {
             changeStep: this.changeStep,
-            content: this.props.content,
-            updateNote: this.props.updateNote
-        }
+            content,
+            editing,
+            updateNote
+        };
         const step = currentStep.getContent(stepProps);
         const finalStep = stepName === 'note' || stepName === 'list';
-
         const noteClass = finalStep ? styles.finalNote : styles.note;
+        
+        const headerProps = {
+            editing,
+            listName: name,
+            onEditClick: this.handleEditingChange,
+            onNameChange: this.listNameChange
+        };
+
+        const stepContent = finalStep ? renderFinalNote(headerProps, step) : step;
 
         return <div className={noteClass}>
-            {step}
+            {stepContent}
         </div>;
     }
 }
+
+const renderHeader = props => {
+    const { editing, listName, onEditClick, onNameChange } = props;
+
+    const headerStyles = classnames(utilStyles.spanHeader, styles.noteHeader,
+        editing ? styles.noteHeaderEditing : '');
+
+    return <div className={headerStyles}>
+        {editing && <span>Editing - </span>}
+        <input type="text" className={classnames(styles.listName, editing ? styles.listNameEditing : '')} 
+            value={listName} disabled={!editing} onChange={onNameChange} />
+        <Icon iconClass="far fa-edit" noWrapper={true} 
+            iconStyles={classnames(styles.editNoteButton, editing ? styles.editNoteButtonActive : '')} 
+            onClick={onEditClick} />
+    </div>;
+};
+
+const renderFinalNote = (headerProps, step) => {
+    return <React.Fragment>
+        {renderHeader(headerProps)}
+        {step}
+    </React.Fragment>;
+};
--- app/components/notesApp/steps/noteType.js
+++ app/components/notesApp/steps/noteType.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 import styles from '../notes.less';
-import utilStyles from '../../utilities/utilities.less';
 
 export const NoteType = (props) => {
     return <div className={styles.noteType}>
-        <div className={utilStyles.spanHeader}>Note</div>
         <div className={styles.noteContainer}>
             <div className={styles.noteContent} contentEditable={true}></div>
         </div>
